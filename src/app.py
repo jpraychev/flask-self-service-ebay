1a4a6cc8fcc0161d8a497806ec7e045d2b6d22bd
Jordan Raychev <jpraychev@gmail.com>
2022-01-31 11:42:57 +0200

    app variables extracted to config file

--- src/app.py
+++ src/app.py
@@ -3,22 +3,12 @@ from flask import render_template
 from flask import request
 from flask import send_from_directory, send_file
 from werkzeug.utils import secure_filename
-from pathlib import Path
 import subprocess
 import io
 import os
-
+import config as cfg
 app = Flask(__name__)
-upload_folder = 'uploads'
-download_folder = 'downloads'
-download_fname = 'ebay_export.csv'
-upload_fname = 'shopify_import.csv'
-convert_script = 'scripts/ebay_import/app.py'
-base_dir = Path(__file__).parent
-# upload_dir = base_dir.joinpath(upload_folder)
-uploaded_file = base_dir.joinpath(upload_folder, upload_fname)
-download_file = base_dir.joinpath(download_folder, download_fname)
-# download_dir = base_dir.joinpath('download/ebay_import.csv')
+
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -30,10 +20,9 @@ def index():
         if 'file-upload' not in request.files:
             return
         f = request.files.get('file-upload')
-        fname = secure_filename(f.filename)
-        f.save(uploaded_file)
-        os.chdir(base_dir)
-        cmd = f'python {convert_script}'
+        f.save(cfg.uploaded_file)
+        os.chdir(cfg.base_dir)
+        cmd = f'python {cfg.convert_script}'
         try:
             subprocess.run(cmd, shell=True, check=True)
         except Exception as e:
@@ -43,12 +32,12 @@ def index():
 @app.route('/download', methods=['GET'])
 def downloads():
     download_data = io.BytesIO()
-    with open(download_file, 'rb') as f:
+    with open(cfg.download_file, 'rb') as f:
         download_data.write(f.read())
         download_data.seek(0)
 
-    download_file.unlink()
-    return send_file(download_data, mimetype='text/csv', download_name=download_fname)
+    cfg.download_file.unlink()
+    return send_file(download_data, mimetype='text/csv', download_name=cfg.download_fname)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ src/config.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+upload_folder = 'uploads'
+download_folder = 'downloads'
+download_fname = 'ebay_export.csv'
+upload_fname = 'shopify_import.csv'
+convert_script = 'scripts/ebay_import/app.py'
+base_dir = Path(__file__).parent
+uploaded_file = base_dir.joinpath(upload_folder, upload_fname)
+download_file = base_dir.joinpath(download_folder, download_fname)
