1471d20be90097ea4d505da0e483d8decac52e35
Jordan Raychev <jpraychev@gmail.com>
2022-02-02 09:57:45 +0200

    added countdown of 60 to success page

--- src/static/api.js
+++ src/static/api.js
@@ -1,65 +1,81 @@
-const removeDisplay = (item) => {
-    $(`${item}`).css('display', 'none')
-    $('#email-address').val('')
+/**
+* @summary Download countdown timer.
+* @param {int} step - Decrease step of the countdown timer
+*/
+
+const downloadCountdown = (step) => {
+    timer = $('#download_timer')
+    currTime = parseInt(timer.html())
+    timer.html(currTime - step)
+    if (currTime <= 1) {
+        clearInterval(timerInterval);
+    } 
 }
+const timerInterval = setInterval(downloadCountdown, 1000, 1);
 
-const addDisplay = (triggerItem, resultItem, content) => {
-    $(`${triggerItem}`).css('display', 'block')
-    $(`${resultItem}`).html(content)
-}
 
-$('.close').on('click', () => {
-    removeDisplay('#myModal');
-});
+// const removeDisplay = (item) => {
+//     $(`${item}`).css('display', 'none')
+//     $('#email-address').val('')
+// }
 
-$(window).on('click', (e) => {
-    if (e.target.id === 'myModal') {
-        removeDisplay('#myModal');
-    };
-});
+// const addDisplay = (triggerItem, resultItem, content) => {
+//     $(`${triggerItem}`).css('display', 'block')
+//     $(`${resultItem}`).html(content)
+// }
 
-$("#email-button").on("click", () => {
-    const email_addr = $("#email-address").val()
-    if (!email_addr) {
-        addDisplay(
-                    triggerItem='#myModal',
-                    resultItem='#modal-text',
-                    content='Email field is empty!')
-        return 
-    }
-    userOnboarding(url='http://127.0.0.1:5000/onboard', email=email_addr);
-});
+// $('.close').on('click', () => {
+//     removeDisplay('#myModal');
+// });
 
-const userOnboarding = async (url, email) => {        
-    var myHeaders = new Headers()
-    myHeaders.append("Content-Type", "application/json")
+// $(window).on('click', (e) => {
+//     if (e.target.id === 'myModal') {
+//         removeDisplay('#myModal');
+//     };
+// });
 
-    var raw = JSON.stringify({
-        "email": email
-    })
+// $("#email-button").on("click", () => {
+//     const email_addr = $("#email-address").val()
+//     if (!email_addr) {
+//         addDisplay(
+//                     triggerItem='#myModal',
+//                     resultItem='#modal-text',
+//                     content='Email field is empty!')
+//         return 
+//     }
+//     userOnboarding(url='http://127.0.0.1:5000/onboard', email=email_addr);
+// });
 
-    var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-    }
+// const userOnboarding = async (url, email) => {        
+//     var myHeaders = new Headers()
+//     myHeaders.append("Content-Type", "application/json")
+
+//     var raw = JSON.stringify({
+//         "email": email
+//     })
+
+//     var requestOptions = {
+//         method: 'POST',
+//         headers: myHeaders,
+//         body: raw,
+//         redirect: 'follow'
+//     }
     
-    try {
-        const response = await fetch(url, requestOptions)
-        if (response.status === 200) {
-            const result = await response.text()
-            if (result !== "0") {
-                addDisplay(
-                    triggerItem='#myModal', 
-                    resultItem='#modal-text', 
-                    content=`Something went wrong in the backend. Status code ${result}`);
-                return
-            }
-            addDisplay(triggerItem='#myModal', resultItem='#modal-text', content=`${email} has been onboarded`);
-        }
-    }
-    catch (error) {
-        console.log(error)
-    }
-}
+//     try {
+//         const response = await fetch(url, requestOptions)
+//         if (response.status === 200) {
+//             const result = await response.text()
+//             if (result !== "0") {
+//                 addDisplay(
+//                     triggerItem='#myModal', 
+//                     resultItem='#modal-text', 
+//                     content=`Something went wrong in the backend. Status code ${result}`);
+//                 return
+//             }
+//             addDisplay(triggerItem='#myModal', resultItem='#modal-text', content=`${email} has been onboarded`);
+//         }
+//     }
+//     catch (error) {
+//         console.log(error)
+//     }
+// }
