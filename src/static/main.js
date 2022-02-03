2fe96d6e7db90191448d0a61b98007521ad0fb54
Jordan Raychev <jpraychev@gmail.com>
2022-02-03 06:48:42 +0200

    api.js renamed to main.js, added additional docstring and cleaned up

---
+++ src/static/main.js
@@ -0,0 +1,24 @@
+/** 
+* @summary Update HTML element with id of note, after file is being deleted from the system
+*/
+const udpateNote = () => {
+    $('#note').html('Note: file has been deleted')
+}
+
+/**
+* @summary Download counter indicating to user how much time they have got to download
+their file. Once the timer expires, we update the note to the user, clean up (stop) the setted interval
+and redirect the user directyly to home page to avoid generating new files.
+* @param {int} step - Decrease step of the countdown timer
+*/
+const downloadCountdown = (step) => {
+    timer = $('#download_timer')
+    currTime = parseInt(timer.html())
+    timer.html(currTime - step)
+    if (currTime <= 1) {
+        udpateNote()
+        clearInterval(timerInterval)
+        windows.location.href = '/'
+    } 
+}
+const timerInterval = setInterval(downloadCountdown, 1000, 1)
