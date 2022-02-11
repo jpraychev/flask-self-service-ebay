3b2bdae29d260cfe95a1847d7e661fa405d00ddb
Jordan Raychev <jpraychev@gmail.com>
2022-02-11 06:30:23 +0200

    ebay script added to project

---
+++ src/scripts/ebay_import/app.py
@@ -0,0 +1,333 @@
+import sys
+import ean.utils as ean_utils
+import sys
+import pandas as pd
+import numpy as np
+from utils import header, export, rooms, models, cm_to_foot
+from html import html_template
+from rows import MainRow, VariantRow
+from material import Material
+import re
+from pathlib import Path
+
+
+def drop_attr(string):
+    """ Drops Color_ and Size_ attributes from a string using a regex expression.
+    This function replaces usage of multiple replace() calls """
+    txt = re.sub('(Size_)|(Color_)', '', string)
+    sep_txt = txt.replace(',', ';', 1)
+    return sep_txt
+
+def size_cm_to_ft(sizes):
+
+    temp_arr = list()
+    if isinstance(sizes, str):
+        sizes = sizes.split(',')
+
+    for size in sizes:
+        size = size.strip()
+        if 'x' in size:
+            splitted = size.replace('cm','').split('x')
+            w_cm, l_cm = splitted[0], splitted[1]
+            w_ft, l_ft = cm_to_foot[w_cm], cm_to_foot[l_cm]
+            string = f'{w_cm}x{l_cm}cm ({w_ft}x{l_ft})'
+            temp_arr.append(string)
+        else:
+            splitted = size.split('cm')
+            size_cm, size_type = splitted[0].strip(), splitted[1].strip()
+            string = f'{size_cm}cm ({cm_to_foot[size_cm]}) {size_type.capitalize()}'
+            temp_arr.append(string)
+
+    return temp_arr
+
+def get_relation_details(row) -> str:
+    """ Tags for a shopify product should contain one of the following structures,
+    anything else should be discouraged:
+    - Size_120x170 cm;
+    - Size_120x170cm;
+    - Size_200 cm round;
+    - Size_200cm round;
+    """
+
+    tags = row[1].to_dict()['Tags']
+    flat_tags = [tag.replace(' cm', 'cm') for tag in tags if tag is not np.NaN]
+    joined_flat_tags = ','.join(flat_tags)
+
+    if not 'cm' in joined_flat_tags:
+        raise ValueError('"cm" is missing for product size')
+
+    flat_wo_tags = drop_attr(joined_flat_tags).split(';')
+    color = flat_wo_tags[0].strip()
+    size = flat_wo_tags[1].strip()
+    
+    if INCH_FLAG:
+        sizes_ft = size_cm_to_ft(size)
+        size = ', '.join(sizes_ft)
+
+    tags = f'Color={color}|Size={size}'
+    return tags
+
+def get_title(row) -> str:
+    title = row[1].to_dict()['Title']
+    flat_title = [val for val in title if val is not np.NaN][0]
+    if len(flat_title) > 80:
+        raise ValueError("Title's length has to be below 80 characters\n", flat_title, len(flat_title))
+    return flat_title
+
+def get_images(row) -> str:
+    urls = [
+        'https://rugsdropshipping.uk/wp-content/uploads/export/xrug/xrug_five.jpg',
+        'https://rugsdropshipping.uk/wp-content/uploads/export/xrug/xrug_size.jpg'
+    ]
+    images = row[1].to_dict()['Image Src']
+    flat_images = [val for val in images if val is not np.NaN]
+    [flat_images.append(url) for url in urls]
+    join_images = ','.join(flat_images).replace(',','|')
+    return (join_images)
+
+def get_color(row) -> str:
+    tags = row[1].to_dict()['Tags']
+    flat_tags = [val.replace(' ', '') for val in tags if val is not np.NaN]
+    drop_comma = [val.replace(',', ';', 1) for val in flat_tags]
+    color = drop_comma[0].split(';')[0].split('_')[1]
+    return color
+
+def get_prices(row):
+    prices = row[1].to_dict()['Variant Price']
+    p_arr = [p for p in prices if p >= 0]
+    min_elem = sorted(p_arr)[0]
+    new_prices = list()
+    for price in p_arr:
+        if price == min_elem:
+            price = price * 1.00
+        else:
+            price = price * 1.25
+        new_prices.append(round(price,2))
+    return new_prices
+
+def get_sizes(row, s_type=None):
+    """Sizes located in Option1 Value should have the following structure,
+    anything else should be discouraged:
+        - 80x150 cm;
+        - 80x150cm;
+        - 120 cm round/square;
+        - 120cm round/square;
+    """
+
+    sizes = row[1].to_dict()['Option1 Value']
+
+    flat_size = [size for size in sizes if size is not np.NaN]
+    if not 'cm' in ','.join(flat_size):
+        raise ValueError('"cm" is missing for product size')
+
+    cm_size = [size.replace(' cm','cm') for size in flat_size]
+    if s_type is not None:
+        return cm_size
+    final_size = cm_size if not INCH_FLAG else size_cm_to_ft(cm_size)
+
+    return final_size
+
+def get_single_variation(row):
+    color = get_color(row)
+    sizes = get_sizes(row)
+    temp_arr = list()
+
+    for size in sizes:
+        temp_arr.append(f"Color={color}|Size={size}")
+    return temp_arr
+
+def get_ean() -> int:
+    if isinstance(DRY_RUN, str):
+        dr = eval(DRY_RUN)
+    if dr:
+        return 123456789
+    # if len(ean.unused_numbers) < 100:
+        # raise ValueError('EAN numbers are less than 100.')
+    num = EAN_NUMS.pop()
+    return num 
+
+def get_width_length(row):
+    sizes = get_sizes(row, 'cm')
+    sizes = [size.replace(' ','') for size in sizes if size is not np.NaN]
+    w = [f"{size.split('x')[0]}cm" for size in sizes if 'x' in size]
+    l = [size.split('x')[1] for size in sizes if 'x' in size]
+    widths = (', ').join(w)
+    lengths = (', ').join(l)
+    if len(widths) > 65 or len(lengths) > 65:
+        raise ValueError("Maximum allowed 65 characters for lengths/widths is exceeded.")
+    return widths, lengths
+
+def get_room(product_title:str) -> str:
+    
+    room = list()
+    title = product_title.lower()
+
+    for key, value in rooms.items():
+        if key in title:
+            room.append(value)
+    
+    out_rooms = ', '.join(room) if len(room) > 0 else rooms['livingroom']
+    return out_rooms
+
+def get_sku(row):
+    skus = row[1].to_dict()['Variant SKU']
+    sku_arr = [sku for sku in skus if sku is not np.NaN]
+    return sku_arr
+
+def get_desc_list(row):
+    html = row[1].to_dict()['Body (HTML)'][0]
+    start = html.find('<ul>')
+    end = html.find('</ul>')
+    list_items = html[start:end].replace('<ul>', '').strip()
+    start_p, end_p = '<p> - ', '</p>'
+    desc_list = list_items.replace('<li>', start_p).replace('</li>', end_p)
+    return desc_list
+
+def replace_html(title:str, desc_list:str):
+    lower_title = title.lower()
+
+    if 'kitchen' in lower_title:
+        cat = 'kitchen'
+    elif 'children' in lower_title or 'kids' in lower_title or 'nursery' in lower_title:
+        cat = 'children'
+    elif 'indoor' in lower_title or 'outdoor' in lower_title:
+        cat = 'indoor_outdoor'
+    else:
+        cat = 'livingroom'
+
+    full_html = html_template['full']
+    cat_html = html_template[cat]
+    html = full_html.replace('__RUG_DESC_FULL', cat_html)
+    title_ph = '__RUG_TITLE'
+    desc_ph = '__RUG_DESC_LIST'
+    return html.replace(title_ph, title).replace(desc_ph, desc_list)
+
+def get_model_mpn(product_title:str) -> str:
+    title = product_title.lower()
+    for key, value in models.items():
+        if key in title:
+            return value, value + 'Rug'
+    return '', ''
+
+def get_quantities(row):
+    quantities = row[1].to_dict()['Variant Inventory Qty']
+    qa = [int(q) for q in quantities if q >= 0]
+    return qa
+
+def get_department(product_title:str) -> str:
+    title = product_title.lower().split()
+    childs = ['children', 'child', 'baby', 'girl', 'boy']
+    depart = 'Children' if any(child in title for child in childs) else 'Adults'
+    return depart
+
+def get_material(row:str) -> str:
+    html = row[1].to_dict()['Body (HTML)'][0].lower()
+    mat = Material(html)
+    return mat.get_material()
+
+def category(cat_type):
+    """ Returns rug's category
+    Categories are downloaded from https://www.ebay.co.uk/sh/reports/uploads
+    by generating a template for certain category
+    Kids - Home, Furniture & DIY > Children's Home & Furniture > Home Décor & Organisation > Rugs & Carpets
+    Normal - Home, Furniture & DIY > Rugs & Carpets > Rugs
+    """
+
+    categories = {
+            "regular" : "45510",
+            "kids" : "177062"
+        }
+
+    try:
+        return categories[cat_type.lower()]
+    except KeyError:
+        raise ValueError('No such category. Supported categories: Regular (45510) and Kids (177062).')
+        
+def export_main_rows(df2):
+
+    main_row = MainRow(header)
+
+    for row in df2.iterrows():
+        details = get_relation_details(row)
+        color = get_color(row)
+        title = get_title(row)      
+        images = get_images(row)
+        width, length = get_width_length(row)
+        rooms = get_room(title)
+        desc_list = get_desc_list(row)
+        html_desc = replace_html(title=title, desc_list=desc_list)
+        model, mpn = get_model_mpn(title)
+        depart = get_department(title)
+        material = get_material(row)
+        header['Category ID'].append(category(CAT))
+        header['Relationship details'].append(details)
+        header['Title'].append(title)
+        header['Item photo URL'].append(images)
+        header["Description"].append(html_desc)
+        header["C:Colour"].append(color)
+        header["C:Material"].append(material)
+        header["C:Room"].append(rooms)
+        header["C:Department"].append(depart)
+        header["C:Item Width"].append(width)
+        header["C:Item Length"].append(length)
+        header["C:MPN"].append(mpn)
+        header["C:Model"].append(model)
+
+        main_row.add_empty()
+        main_row.add_static()
+
+def export_variant_rows(df2):
+
+    variant_row = VariantRow(header)
+
+    for row in df2.iterrows():
+        variations = get_single_variation(row)
+        prices = get_prices(row)
+        titles = get_title(row)
+        skus = get_sku(row)
+        quantities = get_quantities(row)
+
+        for q in quantities:
+            header["Quantity"].append(q)
+            
+        for price in prices:
+            header['Start price'].append(price)
+
+        for sku in skus:
+            header["Custom label (SKU)"].append(sku)
+
+        for var in variations:
+            header['Relationship details'].append(var)
+            header['Title'].append(titles)
+            ean = get_ean()
+            header["P:EAN"].append(ean)
+
+            variant_row.add_empty()
+            variant_row.add_static()
+
+def main():
+    p = Path(__file__).resolve().parent.parent.parent
+    input_fname = p.joinpath('uploads/shopify_import.csv')
+    df = pd.read_csv(input_fname)
+    df2 = df.groupby('Handle').agg(lambda x: list(x))
+    export_main_rows(df2)
+    export_variant_rows(df2)
+    output_fname = p.joinpath('downloads/ebay_export.csv')
+    export(header, output_fname)
+
+if __name__ == '__main__':
+
+    dim = sys.argv[sys.argv.index('--dimension')+1]
+    cat = sys.argv[sys.argv.index('--category')+1]
+    dry_run = sys.argv[sys.argv.index('--dry_run')+1]
+    
+    INCH_FLAG = True if 'inch' in dim else False
+    CAT = 'regular' if cat == 'regular' else 'kids'
+    DRY_RUN = dry_run
+
+    curr = Path(__file__).resolve().parent
+    ean_txt = curr.joinpath('ean/ean.txt')
+    EAN_NUMS = ean_utils.load_data(ean_txt)
+    main()
+
+    ean_utils.save_data(fname=ean_txt, items=EAN_NUMS)
---
+++ src/scripts/ebay_import/utils.py
@@ -0,0 +1,218 @@
+header = {
+    "*Action(SiteID=UK|Country=GB|Currency=GBP|Version=1193)": [],
+    "Custom label (SKU)": [],
+    "Category ID": [],
+    "Title": [],
+    "Relationship": [],
+    "Relationship details": [],
+    "P:UPC": [],
+    "P:ISBN": [],
+    "P:EAN": [],
+    "P:EPID": [],
+    "Start price": [],
+    "Quantity": [],
+    "Item photo URL": [],
+    "Condition ID": [],
+    "Description": [],
+    "Format": [],
+    "Duration": [],
+    "Buy It Now price": [],
+    "VAT%": [],
+    "Paypal accepted": [],
+    "Paypal email address": [],
+    "Immediate pay required": [],
+    "Payment instructions": [],
+    "Location": [],
+    "Shipping service 1 option": [],
+    "Shipping service 1 cost": [],
+    "Shipping service 1 priority": [],
+    "Shipping service 2 option": [],
+    "Shipping service 2 cost": [],
+    "Shipping service 2 priority": [],
+    "Max dispatch time": [],
+    "Returns accepted option": [],
+    "Returns within option": [],
+    "Refund option": [],
+    "Return shipping cost paid by": [],
+    "Shipping profile name": [],
+    "Return profile name": [],
+    "Payment profile name": [],
+    "C:Brand": [],
+    "C:Type": [],
+    "C:Regional Design": [],
+    "C:Colour": [],
+    "C:Material": [],
+    "C:Size": [],
+    "C:Style": [],
+    "C:Room": [],
+    "C:Department": [],
+    "C:Item Width": [],
+    "C:Item Length": [],
+    "C:MPN": [],
+    "C:Model": []
+}
+
+rooms = {
+    'basement' : 'Basement',
+    'bathroom' : 'Bathroom',
+    'livingroom' : 'Living room',
+    'living' : 'Living room',
+    'children' : 'Children',
+    'bedroom' : 'Bedroom',
+    'diningroom' : 'Dining room',
+    'indoor' : 'Indoor/Outdoor',
+    'outdoor' : 'Indoor/Outdoor',
+    'kitchen' : 'Kitchen',
+    'nursery' : 'Nursery',
+    'any' : 'Any Room'
+}
+
+models = {
+    "geometric" : "Geometric",
+    "floral" : "Floral",
+    "border" : "Border",
+    "striped" : "Striped",
+    "oriental" : "Oriental",
+    "moroccan" : "Moroccan",
+    "trellis" : "Trellis",
+    "diamond" : "Diamond",
+    "solid" : "Solid",
+    "monochrome" : "Monochrome",
+    "patchwork" : "Patchwork",
+    "vintage" : "Vintage",
+    "retro" : "Retro",
+    "check" : "Check",
+    "zigzag" : "ZigZag",
+    "chevron" : "Chevron",
+    "berber" : "Berber"
+}
+
+duplicate_regex = '^(.*)(\r?\n\1)+$'
+design_removal = 'Design_[\w\d\s]*,'
+inch_removal = """cm\s\([\d'"x]+\)"""
+
+cm_to_foot = {
+    "10" : "0'3''",
+    "20" : "0'7''",
+    "30" : "1'",
+    "40" : "1'3''",
+    "50" : "1'6''",
+    "60" : "2'",
+    "70" : "2'3''",
+    "80" : "2'6''",
+    "90" : "3'",
+    "100" : "3'3''",
+    "110" : "3'6''",
+    "120" : "3'9''",
+    "130" : "4'3''",
+    "140" : "4'6''",
+    "150" : "4'9''",
+    "160" : "5'2''",
+    "170" : "5'6''",
+    "180" : "5'9''",
+    "190" : "6'2''",
+    "200" : "6'6''",
+    "210" : "6'9''",
+    "220" : "7'2''",
+    "230" : "7'5''",
+    "240" : "7'9''",
+    "250" : "8'2''",
+    "260" : "8'5''",
+    "270" : "8'9''",
+    "280" : "9'2''",
+    "290" : "9'5''",
+    "300" : "9'8''",
+    "310" : "10'2''",
+    "320" : "10'5''",
+    "330" : "10'8''",
+    "340" : "11'2''",
+    "350" : "11'5''",
+    "360" : "11'8''",
+    "370" : "12'1''",
+    "380" : "12'5''",
+    "390" : "12'8''",
+    "400" : "13'1''",
+    "410" : "13'5''",
+    "420" : "13'8''",
+    "430" : "14'1''",
+    "440" : "14'4''",
+    "450" : "14'8''",
+    "460" : "15'1''",
+    "470" : "15'4''",
+    "480" : "15'7''",
+    "490" : "16'1''",
+    "500" : "16'4''",
+    "510" : "16'7''",
+    "520" : "17'1''",
+    "530" : "17'4''",
+    "540" : "17'7''",
+    "550" : "18'",
+    "560" : "18'4''",
+    "570" : "18'7''",
+    "580" : "19'",
+    "590" : "19'4''",
+    "600" : "19'7''",
+    "610" : "20'",
+    "620" : "20'3''",
+    "630" : "20'7''",
+    "640" : "21'",
+    "650" : "21'3''",
+    "660" : "21'7''",
+    "670" : "22'",
+    "680" : "22'3''",
+    "690" : "22'6''",
+    "700" : "23'",
+    "710" : "23'3''",
+    "720" : "23'6''",
+    "730" : "24'",
+    "740" : "24'3''",
+    "750" : "24'6''",
+    "760" : "24'9''",
+    "770" : "25'3''",
+    "780" : "25'6''",
+    "790" : "25'9''",
+    "800" : "26'2''",
+    "810" : "26'6''",
+    "820" : "26'9''",
+    "830" : "27'2''",
+    "840" : "27'6''",
+    "850" : "27'9''",
+    "860" : "28'2''",
+    "870" : "28'5''",
+    "880" : "28'9''",
+    "890" : "29'2''",
+    "900" : "29'5''",
+    "910" : "29'9''",
+    "920" : "30'2''",
+    "930" : "30'5''",
+    "940" : "30'8''",
+    "950" : "31'2''",
+    "960" : "31'5''",
+    "970" : "31'8''",
+    "980" : "32'2''",
+    "990" : "32'5''"
+}
+
+def flatten_dict_list(arr:list) -> list:
+    import operator
+    from functools import reduce
+    return reduce(operator.concat, arr)
+
+def export(data, fname:str):
+    import pandas as pd
+    df = pd.DataFrame.from_dict(data)
+    sorted_df = df.sort_values(by=['Title', 'Category ID'], ascending=[True, False])
+    sorted_df.to_csv(fname, index=False)
+
+def transform_to_ul() -> str:
+
+    with open('desc_list.txt') as f:
+        lines = f.readlines()
+
+    a = [line for line in lines if not line.startswith('\n')]
+    b = [line.replace('-','').replace('\n','') for line in a]
+
+    print('<ul>')
+    for line in b:
+        print(f'<li>{line}</li>')
+    print('</ul>')
